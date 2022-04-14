787f4c2f3698a73a35344be92f0a6fa1809877d3
Antonio <49509570+AntoMarr@users.noreply.github.com>
2022-04-14 16:13:37 -0600

    Update ChoresPage.js

--- components/chores/ChoresPage.js
+++ components/chores/ChoresPage.js
@@ -1,8 +1,9 @@
 import React, { useReducer, useState } from 'react'
-import { FlatList, SectionList, StyleSheet, Text, View, Pressable } from 'react-native'
+import { SectionList, StyleSheet, Text, View, Pressable, Modal } from 'react-native'
 import { MaterialIcons } from '@expo/vector-icons';
+import FormInput from '../../assets/FormInput';
+import MainButton from '../../assets/MainButton';
 import Colors from '../../constants/Colors';
-import CheckBox from './CheckBox'
 
 function ChoresPage() {
 
@@ -95,10 +96,60 @@ function ChoresPage() {
         )
     }
 
+    const [modalVisible, setModalVisible] = useState(false)
+    const [modalText, setModalText] = useState()
+
     return (
         <View>
+            <Modal
+                animationType="fade"
+                transparent={true}
+                visible={modalVisible}
+                onRequestClose={() => {
+                    Alert.alert("Modal has been closed.");
+                    setModalVisible(!modalVisible);
+                }}>
+                <View style={styles.centeredView}>
+                <View style={styles.modalView}>
+                    <Text style={styles.modalText}>Add An Item</Text>
+                    <View style={styles.modalTextInputContainer}>
+                        <FormInput
+                        onChangeText={setModalText}
+                        value={modalText}/>
+                    </View>
+                    <View style={styles.modalButtons}>
+                        <MainButton
+                        action={() => {
+                            if (!modalText.trim() == "")
+                            {
+                                DATA[0].data.push(modalText.trim())
+                                handleClick()
+                                setModalVisible(!modalVisible);
+                                setModalText("")
+                            }
+                        }}>
+                            Submit
+                        </MainButton>
+                        <MainButton
+                        action={() => {
+                            setModalVisible(!modalVisible);
+                            setModalText("")
+                        }}>
+                            Cancel
+                        </MainButton>
+                    </View>
+                </View>
+                </View>
+            </Modal>
+            <MainButton
+            action={() => 
+            {
+                setModalVisible(!modalVisible)
+            }}>Add Task</MainButton>
+            <View style={[styles.divider, {marginTop: 20}]}/>
             <SectionList
                 sections={DATA}
+                style={{height: '90%'}}
                 keyExtractor={(item, index) => item + index}
                 renderItem={(item, index, section) => renderItem(item, index, section)}
                 renderSectionHeader={({ section: { title } }) => (
@@ -150,7 +201,61 @@ const styles = StyleSheet.create({
         alignSelf: 'center',
         marginBottom: 25,
         opacity: 1
-    }
+    },
+
+    centeredView: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: 22
+      },
+      modalView: {
+        width: '80%',
+        height: 270,
+        margin: 20,
+        backgroundColor: "white",
+        borderRadius: 20,
+        padding: 35,
+        alignItems: "center",
+        shadowColor: "#000",
+        shadowOffset: {
+          width: 0,
+          height: 2
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5
+      },
+      button: {
+        borderRadius: 20,
+        padding: 10,
+        elevation: 2,
+      },
+      buttonClose: {
+        backgroundColor: Colors.light.mainColor,
+      },
+      textStyle: {
+        color: "white",
+        fontWeight: "bold",
+        textAlign: "center",
+        fontSize: 24
+      },
+      modalText: {
+        marginBottom: 15,
+        textAlign: "center",
+        fontSize: 24,
+        fontWeight: 'bold'
+      },
+      modalTextInputContainer: {
+          width: '100%',
+          marginBottom: 25,
+          marginTop: -25
+      },
+      modalButtons: {
+          flex: 1,
+          flexDirection: 'row',
+          marginTop: -20
+      }
 })
 
 export default ChoresPage
